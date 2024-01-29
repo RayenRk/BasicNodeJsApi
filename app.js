0f463f1e36f99f22b160a3d0daf954601dcd41cc
RayenRk <rakkadrayen@gmail.com>
2024-01-29 19:39:08 +0100

    Port variable

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.get('/example1', (req, res) => {
 })
 
 // Port configuration
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}...`)
+}); 
